cb855480971050100adc08ad98cbfdd2f2be84f8
Axel-Cruz <axelcruz1806@gmail.com>
2026-04-07 19:16:00 -0300

    Iniciando Exercicio de loja em react

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import "./App.css"; //importando o css para o app
+
+//Para usar os icones do react-icons, primeiro tem que instalar a biblioteca com o comando npm install react-icons
+//depois é só importar o icone que deseja usar, no caso o FaRegUser e o RiLockPasswordLine
+import { FaRegUser } from "react-icons/fa"; //Importando o icone do usuario para o input
+import { RiLockPasswordLine } from "react-icons/ri"; //Importando o icone password para o input
+
+function App() {
+  // agora mudou o class para className para chamar no css
+  // Para mudar o tamanho e a cor do icone escreve assim <FaRegUser color="#ccc" size={20} />
+  return (
+    <div className="container_home">
+      <div className="container_esquerda">
+        <h2>Welcome Back</h2>
+        <p>you can sign in to acess with yout existing account.</p>
+      </div>
+      <div className="container_direita">
+        <h1>Sign In</h1>
+
+        <div className="container_input">
+          <FaRegUser color="#ccc" size={20} />
+          <input placeholder="User or Email"/>
+        </div>
+        <div className="container_input">
+          <RiLockPasswordLine color="#ccc" size={20} />
+          <input placeholder="Password"/>
+        </div>
+      
+         
+      </div>
+    </div>
+  );
+}
+
+export default App; //
